e1085acd854753a197d95bd416ac60032e1c707f
RobinLandraud <robin.landraud@epitech.eu>
2024-05-01 17:53:48 +0900

    [ADD] airquality documentation

--- midterm/airquality.py
+++ midterm/airquality.py
@@ -2,6 +2,17 @@ import requests
 import json
 
 def get_air_quality(lat, long, api_key):
+    """
+    Get the air quality index of a location using the OpenWeatherMap Air Pollution API.
+
+    Parameters:
+        lat (float): The latitude of the location.
+        long (float): The longitude of the location.
+        api_key (str): The API key for OpenWeatherMap.
+
+    Returns:
+        int: The air quality index of the location.
+    """
     url = f"http://api.openweathermap.org/data/2.5/air_pollution?lat={lat}&lon={long}&appid={api_key}"
     response = requests.get(url)
     data = response.json()
